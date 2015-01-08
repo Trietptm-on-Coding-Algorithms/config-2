6e72c58dea9d86bd67b6c5e9694616bfe218f7ee
Zach Riggle <zach@riggle.me>
2015-01-07 22:50:56 -0800

    wat

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -40,8 +40,8 @@ c.InteractiveShell.separate_in = ''
 c.InteractiveShell.separate_out = ''
 c.InteractiveShell.separate_out2 = ''
 c.PromptManager.justify = False
-c.TerminalIPythonApp.nosep = True
-c.IPythonTerminalApp.display_banner = False
+c.InteractiveShell.nosep = True
+c.InteractiveShell.display_banner = False
 
 # Enable GUI event loop integration with any of ('glut', 'gtk', 'gtk3', 'none',
 # 'osx', 'pyglet', 'qt', 'qt4', 'tk', 'wx').
