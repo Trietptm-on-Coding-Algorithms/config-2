b3a0fa89ad5706734f792a991dc04ff80c5aa5b4
Zach Riggle <zachriggle@users.noreply.github.com>
2015-01-07 23:43:42 -0800

    No more SO_REUSEADDR nonsense

--- bin/initialize-linux-vm.sh
+++ bin/initialize-linux-vm.sh
@@ -156,6 +156,12 @@ APT::Periodic::AutocleanInterval "7";
 APT::Periodic::Unattended-Upgrade "1";
 EOF
 
+sudo tee /etc/sysctl.d/10-so_reuseaddr.conf <<EOF
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_tw_recycle = 1
+EOF
+sudo sysctl --system
+
 #
 # Required for 'nc -e'
 #
