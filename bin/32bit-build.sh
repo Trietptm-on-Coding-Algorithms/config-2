4bd04b5515c19a8139d630fff057f9b371434377
Zach Riggle <riggle@google.com>
2015-04-13 13:51:11 -0400

    Update 32-bit build

--- bin/32bit-build.sh
+++ bin/32bit-build.sh
@@ -1,3 +1,9 @@
+
+[ -z "$CC" ] || export CC="$CC -m32"
+[ -z "$CXX" ] || export CC="$CXX -m32"
 export CFLAGS=-m32
 export CXXFLAGS=-m32
 export LDFLAGS=-m32
+
+echo May also want to:
+echo ./configure --build=i686-pc-linux-gnu
