f37960a7f3656482b6c18b1d7f85df756113ff36
Zach Riggle <riggle@google.com>
2016-02-20 23:04:45 -0500

    Update bin

--- bin/gdbserver-build.sh
+++ bin/gdbserver-build.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 set -ex
+
+sudo apt-get build-dep gdb
+
+dir=$(mktemp -d)
+pushd $dir
+
+wget https://ftp.gnu.org/gnu/gdb/gdb-7.10.tar.xz
+tar xf gdb-7.10.tar.xz
+
+cd gdb-7.10
+
 export LDFLAGS=-static
 ./configure --disable-inprocess-agent --disable-host-shared --disable-multilib --enable-static
 make -j$(nproc)
-cp ./gdb/gdbserver/gdbserver .
+cp ./gdb/gdbserver/gdbserver ..
+
+cd ..
+rm -rf gdb-7.10*
+
 upx -9 gdbserver
+
+popd
+
+echo gdbserver built at $dir/gdbserver
