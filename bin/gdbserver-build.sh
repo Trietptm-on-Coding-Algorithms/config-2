6511ac5557db2781bcc5392f722b4efef15d5144
Zach Riggle <riggle@google.com>
2016-04-14 12:48:39 -0700

    Update gdbserver build scripts; add prebuilt binaries

--- bin/gdbserver-build.sh
+++ bin/gdbserver-build.sh
@@ -1,23 +1,33 @@
 #!/bin/sh
 set -ex
 
+VERSION=7.11
+
 sudo apt-get build-dep gdb
 
 dir=$(mktemp -d)
 pushd $dir
 
-wget https://ftp.gnu.org/gnu/gdb/gdb-7.10.tar.xz
-tar xf gdb-7.10.tar.xz
+wget https://ftp.gnu.org/gnu/gdb/gdb-$VERSION.tar.xz
+tar xf gdb-$VERSION.tar.xz
 
-cd gdb-7.10
+cd gdb-$VERSION
 
 export LDFLAGS=-static
-./configure --disable-inprocess-agent --disable-host-shared --disable-multilib --enable-static
+
+./configure --disable-inprocess-agent \
+            --disable-host-shared \
+            --disable-multilib \
+            --enable-static
+
+# One can also specify the target architecture for gdbserver.
+# --host=aarch64-linux-gnu
+
 make -j$(nproc)
 cp ./gdb/gdbserver/gdbserver ..
 
 cd ..
-rm -rf gdb-7.10*
+rm -rf gdb-$VERSION*
 
 upx -9 gdbserver
 
